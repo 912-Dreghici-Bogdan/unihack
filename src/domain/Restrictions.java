d92289ac5d09147bfe288887d1839718c9c82a15
VasiAlexandru <tk60726@gmail.com>
2023-11-17 21:49:24 +0200

    aaaaaaadderge remote-tracking branch 'origin/main'
    well

--- src/domain/Restrictions.java
+++ src/domain/Restrictions.java
@@ -1,25 +1,2 @@
-package src.domain;
-
-public enum Restrictions {
-    GlutenFree,
-    DairyFree,
-    NutFree,
-    EggFree,
-    Vegan,
-    Vegetarian,
-    ShellfishFree,
-    SoyFree,
-    SugarFree,
-    LowCarb,
-    LowFat,
-    LowSodium,
-    Paleo,
-    Pescatarian,
-    Ketogenic,
-    Halal,
-    Kosher,
-    Organic,
-    NonGMO,
-    NoAddedSugar;
-
+package src.domain;public class Restrictions {
 }
