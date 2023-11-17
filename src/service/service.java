e789a50cf19cc7d9f8c911a8060db040534510cd
VasiAlexandru <tk60726@gmail.com>
2023-11-17 17:50:45 +0200

    part iii

--- src/service/service.java
+++ src/service/service.java
@@ -1,5 +1,10 @@
 package src.service;
-
+import src.domain.*;
+import src.repository.*;
 public class service {
-
+    private DiseaseRepo diseases;
+    private IngredientsRepo ingredients;
+    private UserRepo users;
+    private ProductRepo products;
+    private MedicamentationRepo medicamentationRepo;
 }
